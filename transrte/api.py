ea01001c8b50f457e1227611b32748b7f94ca6cc
shuheilocale <default@default.com>
2023-01-07 21:59:39 +0900

    Update dst lang

--- transrte/api.py
+++ transrte/api.py
@@ -3,9 +3,9 @@ import argparse
 from srt_file import SrtFile
 from translator import Translator
 
-def run(in_path, out_path, auth_key):
+def run(in_path, out_path, auth_key, dst_lang):
 
-    translator = Translator(auth_key)
+    translator = Translator(auth_key, dst_lang)
     srt_file = SrtFile(in_path)
 
     for idx in range(len(srt_file)):
@@ -24,6 +24,7 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--input', type=str)
     parser.add_argument('-o', '--output', type=str)
     parser.add_argument('-a', '--auth_key', type=str)
+    parser.add_argument('-l', '--dst_lang', type=str, default='JA')
     args = parser.parse_args()
 
-    run(args.input, args.output, args.auth_key)
+    run(args.input, args.output, args.auth_key, args.dst_lang)
